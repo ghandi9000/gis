54b3941b0d7b5be01de08ec6c2929840f4eb68f7
Noah Peart <noah.v.peart@gmail.com>
2015-02-06 11:52:40 -0500

    Batch update

--- test.R
+++ test.R
@@ -3,7 +3,7 @@
 ## Description: Testing reading gis output
 ## Author: Noah Peart
 ## Created: Fri Jan 30 14:46:38 2015 (-0500)
-## Last-Updated: Fri Jan 30 15:06:19 2015 (-0500)
+## Last-Updated: Fri Feb  6 11:52:25 2015 (-0500)
 ##           By: Noah Peart
 ######################################################################
 
@@ -11,9 +11,18 @@ dat <- read.table('plot5attri.txt', header=T, sep=",")
 dat <- read.csv('plot5attri.txt', header=T)
 
 library(plotrix)
+save <- FALSE
 
-png(filename = "plot5.png")
+if (save)
+    png(filename = "plot5.png")
+    
 par(mfrow = c(2, 1))
 polar.plot(dat$ZENITH_ANG, dat$HORIZ_ANG, main="Horizontal angle, radius is zenith angle")
 polar.plot(dat$DIST_3D, dat$HORIZ_ANG, main="Horizontal angle, radius is 3D distance")
-dev.off()
+
+if (save)
+    dev.off()
+
+## Read all
+files <- list.files('./SkylineOutput', full.names = TRUE)
+tst <- lapply(files, read.csv)
